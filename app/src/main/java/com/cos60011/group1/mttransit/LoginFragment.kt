148e3c08994f92da04cb1a3a6ba4cb67e662e3a2
David He <chuanheau@gmail.com>
2021-09-26 21:01:31 +1000

    Create bus status UI Add navigation test from LoginFragment to BusStatusFragment

--- app/src/main/java/com/cos60011/group1/mttransit/LoginFragment.kt
+++ app/src/main/java/com/cos60011/group1/mttransit/LoginFragment.kt
@@ -6,6 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
 import com.cos60011.group1.mttransit.databinding.FragmentLoginBinding
 
 
@@ -16,6 +18,10 @@ class LoginFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val binding: FragmentLoginBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_login, container, false)
+
+        binding.buttonLogin.setOnClickListener { view: View ->
+            view.findNavController().navigate(R.id.action_loginFragment_to_busStatusFragment)
+        }
         return binding.root
     }
 
