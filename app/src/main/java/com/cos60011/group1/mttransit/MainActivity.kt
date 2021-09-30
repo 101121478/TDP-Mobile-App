2227c709b78ee4140d0b0a61bcc00a7a63ff3d68
David He <chuanheau@gmail.com>
2021-09-30 11:01:15 +1000

    Add top action bar and navigate up function

--- app/src/main/java/com/cos60011/group1/mttransit/MainActivity.kt
+++ app/src/main/java/com/cos60011/group1/mttransit/MainActivity.kt
@@ -4,17 +4,24 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.cos60011.group1.mttransit.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-      /*
-      val binding = ActivityMainBinding.inflate(layoutInflater)
-       setContentView(binding.root)
-       */
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
 
+        // Link the NavController to the actionBar
+        val navController = this.findNavController(R.id.app_nav_host_fragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    // enable navigateUp in nav controller
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.app_nav_host_fragment)
+        return navController.navigateUp()
     }
 }
