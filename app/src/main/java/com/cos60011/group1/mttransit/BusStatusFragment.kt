089ccb96d364454d02123a54259959acf6f3feaa
David He <chuanheau@gmail.com>
2021-10-04 20:45:45 +1100

    Redesign UI

--- app/src/main/java/com/cos60011/group1/mttransit/BusStatusFragment.kt
+++ app/src/main/java/com/cos60011/group1/mttransit/BusStatusFragment.kt
@@ -5,8 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.ui.graphics.Color
 import androidx.databinding.DataBindingUtil
 import com.cos60011.group1.mttransit.databinding.FragmentBusStatusBinding
+import com.google.firebase.database.collection.LLRBNode
 
 class BusStatusFragment : Fragment() {
 
@@ -16,7 +18,6 @@ class BusStatusFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val binding: FragmentBusStatusBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_bus_status, container, false)
-
         return binding.root
     }
 
