5e36dc3dd3f8ec94d17e193ddd483f0c52ef7fc7
Luke <luke32246@gmail.com>
2021-10-05 20:16:44 +1100

    arrival time button onclick listener to test setting arrival time in Firestore

--- app/src/main/java/com/cos60011/group1/mttransit/BusStatusFragment.kt
+++ app/src/main/java/com/cos60011/group1/mttransit/BusStatusFragment.kt
@@ -7,8 +7,11 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import com.cos60011.group1.mttransit.databinding.FragmentBusStatusBinding
+import com.cos60011.group1.mttransit.firestore.FirestoreClass
 
 class BusStatusFragment : Fragment() {
+
+    private val firestore = FirestoreClass()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -16,6 +19,11 @@ class BusStatusFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val binding: FragmentBusStatusBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_bus_status, container, false)
+
+        binding.buttonBusStatusArrive.setOnClickListener { view: View ->
+
+            firestore.setArrivalTime("5", "")
+        }
 
         return binding.root
     }
