3f768d6ec02968b9d493489826b6d33d8664cde9
Luke <luke32246@gmail.com>
2021-10-08 18:28:27 +1100

    on success & no internet alerts & navigation back to dashboard

--- app/src/main/java/com/cos60011/group1/mttransit/BusEntryFragment.kt
+++ app/src/main/java/com/cos60011/group1/mttransit/BusEntryFragment.kt
@@ -1,5 +1,9 @@
 package com.cos60011.group1.mttransit
 
+import android.app.AlertDialog
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -7,13 +11,17 @@ import com.cos60011.group1.mttransit.databinding.FragmentBusEntryBinding
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
+import com.google.firebase.Timestamp
 import com.google.firebase.firestore.DocumentReference
 import com.google.firebase.firestore.FirebaseFirestore
+import java.util.*
 
 
 class BusEntryFragment : Fragment() {
 
     private val projectFirestore = FirebaseFirestore.getInstance()
+
 
     override fun onCreateView( inflater: LayoutInflater, container: ViewGroup?,
                                 savedInstanceState: Bundle?): View? {
@@ -26,6 +34,7 @@ class BusEntryFragment : Fragment() {
             val route = binding.routeSelectionSpinner.selectedItem.toString()
             val busCapacity = Integer.parseInt(binding.passengerCapacityInput.text.toString())
             val passengersOnboard = Integer.parseInt(binding.passengersOnboardInput.text.toString())
+            val initArrivalTime = Timestamp(Date())
 
             val busRoutes = hashMapOf(
                 "Sandringham To CBD" to "/routes/sandringhamToCBD",
@@ -35,8 +44,12 @@ class BusEntryFragment : Fragment() {
             )
             val routeRef: DocumentReference = projectFirestore.document(busRoutes.get(route)!!)     //Create a document reference of the bus route
             // Hashmap of bus info that will be stored in Firestore
+            // TODO: Update hashMap to new data structure
+            // TODO: Get the user selected station and store in the hashmap as initial bus location
             val bus = hashMapOf(
-                "arrivalTime" to emptyMap<String, String>(),
+                "arrivalTime" to hashMapOf(
+                    "time" to initArrivalTime,
+                ),
                 "busLocation" to emptyMap<String, String>(),
                 "busType" to busType,
                 "capacity" to busCapacity,
@@ -51,13 +64,61 @@ class BusEntryFragment : Fragment() {
             println("BUS CAPACITY: " + busCapacity)
             println("PASSENGERS ONBOARD: " + passengersOnboard)
 
+
+            // Check if device has internet connection. If not then display alert informing user
+            if(!checkInternetConnection()){
+                val builder = AlertDialog.Builder(context)
+                builder.setTitle("No Internet Connection")
+                builder.setMessage("Data will be stored when connection is re-established")
+                builder.setPositiveButton("OK"){_,_ ->
+                    view.findNavController().navigate(R.id.action_busEntryFragment_to_busBoardFragment)
+                }
+                builder.show()
+            }
+
             //Store the new bus as a document in the buses collection in Firestore project
+            //TODO: Update code to set the correct data based on the new database structure in Firestore
             projectFirestore.collection("buses").document(busID)
                 .set(bus)
-                .addOnSuccessListener { println("DocumentSnapshot successfully written!") }
-                .addOnFailureListener { e -> Log.w("Error writing document", e) }
+                .addOnSuccessListener {
+                    println("DocumentSnapshot successfully written!")
+                    val builder = AlertDialog.Builder(context)
+                    builder.setTitle("Successfully added bus $busID")
+                    builder.setPositiveButton("OK"){_,_ ->
+                        if(this.isVisible) {
+                            view.findNavController().navigate(R.id.action_busEntryFragment_to_busBoardFragment)
+                        }
+                    }
+                    builder.show()
+                }
+                .addOnFailureListener {
+                        exception -> Log.w("Error writing document", exception)
+                    val builder = AlertDialog.Builder(context)
+                    builder.setTitle("Could not add bus $busID")
+                    builder.setMessage("Error writing document $exception")
+                    builder.setPositiveButton("OK"){_,_ -> }
+                    builder.show()
+                }
         }
         return binding.root
+    }
+
+    // Function to check if the device has an internet conenction either over wifi or mobile data
+    fun checkInternetConnection(): Boolean {
+        val connectivityManager = context?.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        if (connectivityManager != null) {
+            var capabilities =
+                connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
+            if(capabilities != null) {
+                if(capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)){
+                    return true
+                }
+                if(capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)){
+                    return true
+                }
+            }
+        }
+        return false
     }
 
 }
